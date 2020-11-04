58805301b604e9024ab200882334558af2e49a49
experiment-lab <59763282+experiment-lab@users.noreply.github.com>
2020-11-04 11:18:16 +0530

    Update prog.s

--- test/prog.s
+++ test/prog.s
@@ -1,12 +1,20 @@
 .globl main:
 	la $t0 vars
-	lw $a0 4($t0)
+	lw $a0 0($t0)
 	sw $a0 0($sp)
 	addiu $sp $sp -4
 	la $t0 vars
 	lw $a0 8($t0)
+	sw $a0 0($sp)
+	addiu $sp $sp -4
+	li $a0 10
 	lw $t0 4($sp)
-	addiu $sp $sp +4
-	add $a0 $t0 $a0
+	addiu $sp $sp  4
+	mul $a0 $t0 $a0
+	lw $t0 4($sp)
+	addiu $sp $sp  4
+	sub $a0 $t0 $a0
 	la $t0 vars
-	sw $a0 0($t0)
+	sw $a0 12($t0)
+	li	$v0 10	# Code for syscall: exit
+	syscall
